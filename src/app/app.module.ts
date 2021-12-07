e21cb1cfd01c26c0037b50051679282af25d06ba
caroline-calixto <carolinecalixto.santos@gmail.com>
2021-12-07 17:12:23 -0300

    implementação do layout do inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { MenuComponent } from './menu/menu.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { InicioComponent } from './inicio/inicio.component';
     MenuComponent,
     CadastrarComponent,
     EntrarComponent,
-    InicioComponent
+    InicioComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,12 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
